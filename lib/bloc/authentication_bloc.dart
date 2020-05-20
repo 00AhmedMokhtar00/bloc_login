33c5c30b8554a581c448e66a0cea7e23c4bb3cca
Ahmed Mokhtar <ahmedmokhtar7711@gmail.com>
2020-05-20 03:02:27 +0200

    started with bloc

--- lib/bloc/authentication_bloc.dart
+++ lib/bloc/authentication_bloc.dart
@@ -1,15 +1,43 @@
 import 'dart:async';
+
 import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+import '../user_repository.dart';
 import './bloc.dart';
 
 class AuthenticationBloc extends Bloc<AuthenticationEvent, AuthenticationState> {
-  @override
-  AuthenticationState get initialState => InitialAuthenticationState();
+  final UserRepository userRepository;
+
+  AuthenticationBloc({@required this.userRepository}): assert(userRepository != null);
 
   @override
-  Stream<AuthenticationState> mapEventToState(
-    AuthenticationEvent event,
-  ) async* {
-    // TODO: Add Logic
+  AuthenticationState get initialState => AuthenticationUninitialized();
+
+  @override
+  Stream<AuthenticationState> mapEventToState(AuthenticationEvent event) async* {
+    if (event is AppStarted) {
+      final bool hasToken = await userRepository.hasToken();
+
+      if (hasToken) {
+        yield AuthenticationAuthenticated();
+      } else {
+        yield AuthenticationUnauthenticated();
+      }
+    }
+
+    if (event is LoggedIn) {
+      yield AuthenticationLoading();
+      await userRepository.persistToken(event.token);
+      yield AuthenticationAuthenticated();
+    }
+
+    if (event is LoggedOut) {
+      yield AuthenticationLoading();
+      await userRepository.deleteToken();
+      yield AuthenticationUnauthenticated();
+    }
   }
+
 }
+
--- lib/bloc/authentication_state.dart
+++ lib/bloc/authentication_state.dart
@@ -2,9 +2,15 @@ import 'package:equatable/equatable.dart';
 
 abstract class AuthenticationState extends Equatable {
   const AuthenticationState();
-}
-
-class InitialAuthenticationState extends AuthenticationState {
   @override
   List<Object> get props => [];
 }
+
+
+class AuthenticationUninitialized extends AuthenticationState {}
+
+class AuthenticationAuthenticated extends AuthenticationState {}
+
+class AuthenticationUnauthenticated extends AuthenticationState {}
+
+class AuthenticationLoading extends AuthenticationState {}

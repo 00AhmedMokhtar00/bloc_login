927a1369d48d580b05505c39e2faf90c53f193f7
Ahmed Mokhtar <ahmedmokhtar7711@gmail.com>
2020-05-20 04:16:30 +0200

    is about to finish

---
+++ lib/ui/login_page.dart
@@ -0,0 +1,34 @@
+import 'package:bloclogin/blocs/authentication_bloc/bloc.dart';
+import 'package:bloclogin/blocs/login_bloc/bloc.dart';
+import 'package:flutter/material.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../user_repository.dart';
+import 'login_form.dart';
+
+class LoginPage extends StatelessWidget {
+  final UserRepository userRepository;
+
+  LoginPage({Key key, @required this.userRepository})
+      : assert(userRepository != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: BlocProvider(
+        create: (context) {
+          return LoginBloc(
+            authenticationBloc: BlocProvider.of<AuthenticationBloc>(context),
+            userRepository: userRepository,
+          );
+        },
+        child: LoginForm(),
+      ),
+    );
+  }
+}
